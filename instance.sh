c56db79bd8daddc0435afb0f96f3c013e84fbd9f
Lokesh <Lokeshkommini@gmail.com>
2026-05-27 11:44:09 +0530

    added

--- instance.sh
+++ instance.sh
@@ -4,17 +4,15 @@ AMI_ID="ami-0220d79f3f480ecf5"
 ZONE_ID="Z04296232TPHCB0NK6KLW"
 DOMAIN_NAME="robo.lokesh23.online"
 
-instance=$1
-
 for instance in $@
 do
+    echo "Launching instance: $instance"
     INSTANCE_ID=$(aws ec2 run-instances \
-    --image-id $AMI_ID \
-    --instance-type t3.micro \
-    --security-groups "roboshop-common" "robo-$instance" \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
-    --query 'Instances[0].InstanceId' \
-    --output text 
+        --image-id ami-0220d79f3f480ecf5 \
+        --instance-type t3.micro \
+        --security-groups "roboshop-common" "roboshop-$instance" \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
+        --query 'Instances[0].InstanceId' \
+        --output text
     )
-    echo "Instance ID: $INSTANCE_ID"
 done
