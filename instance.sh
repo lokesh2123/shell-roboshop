c3bc029ad61714c1e018c67cb9d4e91bf92d7de6
Lokesh <Lokeshkommini@gmail.com>
2026-05-27 11:36:39 +0530

    added

--- instance.sh
+++ instance.sh
@@ -4,17 +4,16 @@ AMI_ID="ami-0220d79f3f480ecf5"
 ZONE_ID="Z04296232TPHCB0NK6KLW"
 DOMAIN_NAME="robo.lokesh23.online"
 
-NAME=$1
 
-for NAME in $@
+for instance in $@
     do
     aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type t3.micro \
-    --security-groups "roboshop-common" "roboshop-$NAME" \
+    --security-groups "roboshop-common" "roboshop-$instance" \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$NAME}]" \
     --query 'Instances[0].InstanceId' \
     --output text   
     
-    echo "Instance $NAME created successfully"
+    echo "Instance $instance created successfully"
 done
