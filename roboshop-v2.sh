9dcf0d90de2e43b5bd14f243c9e0a4be25231085
joindevopscloud <info@joindevops.com>
2026-05-27 07:31:29 +0530

    api doc added

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -18,7 +18,7 @@ fi
 ACTION=$1
 shift # first argument will be removed
 
-if [ $ACTION -ne "create" ] && [ $ACTION -ne "delete" ]; then
+if [ "$ACTION" -ne "create" ] && [ "$ACTION" -ne "delete" ]; then
     echo -e "$R ERROR:: First argument must be either create or delete"
     echo "USAGE: $0 [create/delete] [instance1] [instance2...]"
     exit 1
