f64dccd5680928fd946fba5076d70b6cff74ad1b
Lokesh <Lokeshkommini@gmail.com>
2026-05-27 11:38:41 +0530

    added

--- instance.sh
+++ instance.sh
@@ -6,14 +6,14 @@ DOMAIN_NAME="robo.lokesh23.online"
 
 
 for instance in $@
-    do
-    aws ec2 run-instances \
+do
+    INSTANCE_ID=$(aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type t3.micro \
     --security-groups "roboshop-common" "roboshop-$instance" \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$NAME}]" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance    }]" \
     --query 'Instances[0].InstanceId' \
-    --output text   
-    
-    echo "Instance $instance created successfully"
+    --output text 
+    )
+    echo "Instance ID: $INSTANCE_ID"
 done
