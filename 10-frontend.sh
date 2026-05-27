7320ac968a7b8f31415604e59a563a132896bff3
joindevopscloud <info@joindevops.com>
2026-05-27 08:37:48 +0530

    api doc added

--- 10-frontend.sh
+++ 10-frontend.sh
@@ -29,17 +29,17 @@ VALIDATE(){
     fi
 }
 
-dnf module disable nginx -y
-dnf module enable nginx:1.24 -y
-dnf install nginx -y
+dnf module disable nginx -y &>> $LOGS_FILE
+dnf module enable nginx:1.24 -y &>> $LOGS_FILE
+dnf install nginx -y &>> $LOGS_FILE
 VALIDATE $? "Installing Nginx"
 
-rm -rf /usr/share/nginx/html/* 
+rm -rf /usr/share/nginx/html/* &>> $LOGS_FILE
 VALIDATE $? "Removed Default code"
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>> $LOGS_FILE
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>> $LOGS_FILE
 VALIDATE $? "Downloaded and extracted frontend code"
 
 rm -rf /etc/nginx/nginx.conf
@@ -49,5 +49,5 @@ cp nginx.conf /etc/nginx/nginx.conf
 VALIDATE $? "Copied roboshop nginx conf"
 
 systemctl restart nginx
-systemctl enable nginx
+systemctl enable nginx &>> $LOGS_FILE
 VALIDATE $? "Enabled and restarted nginx"
