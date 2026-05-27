3039edcc34dda4a509d23181edabc7a978e563b8
Lokesh <Lokeshkommini@gmail.com>
2026-05-27 11:42:07 +0530

    added

--- instance.sh
+++ instance.sh
@@ -4,13 +4,15 @@ AMI_ID="ami-0220d79f3f480ecf5"
 ZONE_ID="Z04296232TPHCB0NK6KLW"
 DOMAIN_NAME="robo.lokesh23.online"
 
+instance=$1
+
 for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type t3.micro \
     --security-groups "roboshop-common" "roboshop-$instance" \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance    }]" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
     --query 'Instances[0].InstanceId' \
     --output text 
     )
